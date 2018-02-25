43ae8c0bca0cb49a67b1d7af181f973d3077a96c
PizzaCrust <pizzacrustcoder@gmail.com>
2018-02-25 18:09:13 +0700

    Basic card implementation

---
+++ src/main/java/online/pizzacrust/trello/impl/BasicCard.java
@@ -0,0 +1,73 @@
+package online.pizzacrust.trello.impl;
+
+import com.google.gson.Gson;
+import com.google.gson.annotations.SerializedName;
+
+import com.mashape.unirest.http.ObjectMapper;
+import com.mashape.unirest.http.Unirest;
+
+import online.pizzacrust.trello.Card;
+
+public class BasicCard extends BasicIdentifiable implements Card {
+
+    private String name;
+
+    @SerializedName("desc")
+    private String description;
+
+    public BasicCard() {}
+
+    private String apiKey;
+
+    private String userToken;
+
+    public BasicCard(String id,
+                     String apiKey,
+                     String userToken) throws Exception {
+        super(id);
+        installMapper();
+        this.apiKey = apiKey;
+        this.userToken = userToken;
+        refresh();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void refresh() throws Exception {
+        String rootUrl = "https://api.trello.com/1/cards/" + id;
+        BasicCard basicCard = Unirest.get(rootUrl).queryString("key", apiKey).queryString
+                ("token", userToken)
+                .asObject(BasicCard.class).getBody();
+        this.description = basicCard.getDescription();
+        this.name = basicCard.getName();
+    }
+
+    public static void main(String... args) throws Exception {
+        BasicCard basicCard = new BasicCard("fHuGaT5h", args[0], args[1]);
+        System.out.println(basicCard.getName());
+        System.out.println(basicCard.getDescription());
+    }
+
+    public static final boolean OBJECT_MAPPER_INSTALLED = false;
+
+    public static void installMapper() {
+        if (!OBJECT_MAPPER_INSTALLED) {
+            Unirest.setObjectMapper(new ObjectMapper() {
+                public <T> T readValue(String s, Class<T> aClass) {
+                    return new Gson().fromJson(s, aClass);
+                }
+
+                public String writeValue(Object o) {
+                    return new Gson().toJson(o);
+                }
+            });
+        }
+    }
+
+}
---
+++ src/main/java/online/pizzacrust/trello/impl/BasicIdentifiable.java
@@ -0,0 +1,21 @@
+package online.pizzacrust.trello.impl;
+
+import online.pizzacrust.trello.Identifiable;
+
+public class BasicIdentifiable implements Identifiable {
+
+    protected String id;
+
+    public BasicIdentifiable() {
+
+    }
+
+    public BasicIdentifiable(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+}
