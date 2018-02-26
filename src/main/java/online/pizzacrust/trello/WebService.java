22b2c21d6598e7ae99f0fdecb48b814aefc042c9
PizzaCrust <pizzacrustcoder@gmail.com>
2018-02-26 23:14:50 +0700

    Use web service

---
+++ src/main/java/online/pizzacrust/trello/WebService.java
@@ -0,0 +1,62 @@
+package online.pizzacrust.trello;
+
+import com.google.gson.Gson;
+
+import com.mashape.unirest.http.ObjectMapper;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.request.GetRequest;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class WebService {
+
+    private final String apiKey;
+    private final String userToken;
+
+    public WebService(String apiKey, String userToken) {
+        Unirest.setObjectMapper(new ObjectMapper() {
+            public <T> T readValue(String s, Class<T> aClass) {
+                return new Gson().fromJson(s, aClass);
+            }
+
+            public String writeValue(Object o) {
+                return new Gson().toJson(o);
+            }
+        });
+        this.apiKey = apiKey;
+        this.userToken = userToken;
+    }
+
+    private Map<String, Object> convertToMap(String[] varargs) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        int stage = 0;
+        String cache = null;
+        for (String vararg : varargs) {
+            if (stage == 0) {
+                cache = vararg;
+                stage = 1;
+                continue;
+            }
+            map.put(cache, vararg);
+            stage = 0;
+            cache = null;
+        }
+        return map;
+    }
+
+    public <T> T getRequest(String rootUrl,
+                            Class<T> clazz,
+                            String... paramNameToParamVal) throws Exception {
+        GetRequest getRequest = Unirest.get(rootUrl);
+        if (apiKey != null && userToken != null) {
+            getRequest.queryString("key", apiKey).queryString
+                    ("token",
+                            userToken);
+        }
+        return getRequest.queryString(convertToMap
+                (paramNameToParamVal)).asObject
+                (clazz).getBody();
+    }
+
+}
--- src/main/java/online/pizzacrust/trello/impl/BasicCard.java
+++ src/main/java/online/pizzacrust/trello/impl/BasicCard.java
@@ -1,14 +1,9 @@
 package online.pizzacrust.trello.impl;
 
-import com.google.gson.Gson;
 import com.google.gson.annotations.SerializedName;
 
-import com.mashape.unirest.http.ObjectMapper;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.request.GetRequest;
-import com.mashape.unirest.request.HttpRequest;
-
 import online.pizzacrust.trello.Card;
+import online.pizzacrust.trello.WebService;
 
 public class BasicCard extends BasicIdentifiable implements Card {
 
@@ -19,17 +14,12 @@ public class BasicCard extends BasicIdentifiable implements Card {
 
     public BasicCard() {}
 
-    private String apiKey;
-
-    private String userToken;
+    private WebService webService;
 
     public BasicCard(String id,
-                     String apiKey,
-                     String userToken) throws Exception {
+                     WebService webService) throws Exception {
         super(id);
-        installMapper();
-        this.apiKey = apiKey;
-        this.userToken = userToken;
+        this.webService = webService;
         refresh();
     }
 
@@ -43,35 +33,15 @@ public class BasicCard extends BasicIdentifiable implements Card {
 
     public void refresh() throws Exception {
         String rootUrl = "https://api.trello.com/1/cards/" + id;
-        HttpRequest request = Unirest.get(rootUrl);
-        if (apiKey != null && userToken != null) {
-            request = request.queryString("key", apiKey).queryString("token", userToken);
-        }
-        BasicCard basicCard = request.asObject(BasicCard.class).getBody();
+        BasicCard basicCard = webService.getRequest(rootUrl, BasicCard.class);
         this.description = basicCard.getDescription();
         this.name = basicCard.getName();
     }
 
     public static void main(String... args) throws Exception {
-        BasicCard basicCard = new BasicCard("fHuGaT5h", args[0], args[1]);
+        BasicCard basicCard = new BasicCard("fHuGaT5h", new WebService(args[0], args[1]));
         System.out.println(basicCard.getName());
         System.out.println(basicCard.getDescription());
-    }
-
-    public static final boolean OBJECT_MAPPER_INSTALLED = false;
-
-    public static void installMapper() {
-        if (!OBJECT_MAPPER_INSTALLED) {
-            Unirest.setObjectMapper(new ObjectMapper() {
-                public <T> T readValue(String s, Class<T> aClass) {
-                    return new Gson().fromJson(s, aClass);
-                }
-
-                public String writeValue(Object o) {
-                    return new Gson().toJson(o);
-                }
-            });
-        }
     }
 
 }
